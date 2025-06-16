a64658195420c37e1c067313e7165f329e166c48
Daniel A Osae <daosae@me.com>
2025-06-16 23:09:07 +0100

    Remove url encoding

--- src/utils/getImageUrlUtil/GetImageUrl.js
+++ src/utils/getImageUrlUtil/GetImageUrl.js
@@ -1,12 +1,20 @@
 export const getImageUrl = (path, options = {}) => {
   if (process.env.NODE_ENV === "production") {
-    const params = new URLSearchParams({
-      url: `${path}`,
-      ...(options.width && { w: options.width }),
-      q: options.quality || 80,
-      fm: options.format || "webp",
-    });
-    return `/.netlify/images?${params}`;
+    const params = new URLSearchParams();
+    params.append("url", path);
+    if (options.width) params.append("w", options.width);
+    params.append("q", options.quality || 80);
+    params.append("fm", options.format || "webp");
+
+    // Decode only the 'url' parameter to avoid %2F in the path
+    const paramString = params
+      .toString()
+      .replace(
+        /url=([^&]*)/,
+        (_, urlValue) => `url=${decodeURIComponent(urlValue)}`
+      );
+
+    return `/.netlify/images?${paramString}`;
   }
 
   return `${path}`;
